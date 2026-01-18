ca02f98e28ec26f22518ac2c100dc539f7d294a4
Pavan Kalisetti <pavan@hacktron.ai>
2026-01-19 00:35:59 +0530

    add name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,6 +15,7 @@ const Navbar = () => {
 
       <div className="logo">
         <img src={logo} alt="medic-logo" />
+        <h1>Sushruta Hospital</h1>
       </div>
 
       <div className={`hamburger ${isNavOpen ? "open" : ""}`} onClick={toggleNav}>
@@ -25,16 +26,16 @@ const Navbar = () => {
 
       <div className={`nav-links ${isNavOpen ? "open" : ""}`}>
         <div className="nav-items">
-            <h3>Medical Care</h3>
-            <h3>Medical Health</h3>
-            <h3>Medical Test</h3>
-            <h3>Medical Lab</h3>
-            <h3>Medical Contact</h3>
+          <h3>Medical Care</h3>
+          <h3>Medical Health</h3>
+          <h3>Medical Test</h3>
+          <h3>Medical Lab</h3>
+          <h3>Medical Contact</h3>
         </div>
 
         <div className="side-nav-items">
-            <h3>Login</h3>
-            <img src={search} alt="search" />
+          <h3>Login</h3>
+          <img src={search} alt="search" />
         </div>
       </div>
 
